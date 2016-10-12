0b7c1caca48ecb453bad2d485fa8f45f0f9fed8a
klashxx <klashxx@gmail.com>
2016-10-12 12:25:40 +0200

    Basic func handling

--- postman.py
+++ postman.py
@@ -2,6 +2,31 @@
 # -*- coding: utf-8 -*-
 """mail sender helper"""
 
+def postman(mboxes,
+            subject,
+            body=None,
+            attach=None,
+            embed=None,
+            poster='noresponse@postman.org',
+            smtp_servers=None,
+            important=False,
+            login=None,
+            passwd=None,
+            logger=None):
+
+    args = locals()
+    import logging
+
+    if not isinstance(logger, logging.Logger):
+        logger = logging.getLogger('postman')
+        logger.setLevel(logging.DEBUG)
+        ch = logging.StreamHandler()
+        ch.setFormatter(logging.Formatter('%(levelname)s - %(message)s'))
+        logger.addHandler(ch)
+
+    logger.debug(args)
+
+    return None
 
 def cli():
     cli_docs = """postman™ ...just another mail sender.
@@ -11,7 +36,7 @@ Usage:
 
 Options:
   --body BODY          body of the mail
-  --poster POSTER      mailbox of the sender [default: noresponse@postman.com]
+  --poster POSTER      mailbox of the sender [default: noresponse@postman.org]
   --attach=<files>     file to attach
   --embed=<files>      file to embed
   --smtp SERVER        smtp server
@@ -21,6 +46,7 @@ Options:
   --version            show program's version number and exit
   -h, --help           show this help message and exit
   """
+
 
     from docopt import docopt, DocoptExit
     from schema import Schema, And, Use, SchemaError, Optional
@@ -45,6 +71,17 @@ Options:
         schema.validate(args)
     except SchemaError:
         raise DocoptExit
+
+    postman(args['--mbox'],
+            args['<subject>'],
+            body=args['--body'],
+            attach=args['--attach'],
+            embed=args['--embed'],
+            poster=args['--poster'],
+            smtp_servers=args['--smtp'],
+            important=args['--important'],
+            login=args['--login'],
+            passwd=args['--passwd'])
 
     return None
 
